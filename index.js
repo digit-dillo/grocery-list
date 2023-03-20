2a292fae690cb65b3e5d2c64407696d7107169dc
Dillo <digit.dillo@gmail.com>
2023-03-19 23:09:29 -0500

    session 3: can remove from DB, can use a favicon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // FIREBASE SETUP ============================================================================= //
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://playground-bf5b1-default-rtdb.firebaseio.com/"
@@ -29,9 +29,25 @@ button.addEventListener('click', function() {
     clearTextBox(textbox);
 });
 
-function addToShoppingList(itemToAdd) {
-    // add to list
-    shoppingList.innerHTML += `<li>${itemToAdd}</li>`;
+function addToShoppingList(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    // create an li element
+    const li = document.createElement("li");
+
+    // add some text
+    li.textContent = itemValue;
+
+    // now that it has been created, have it listen for removal
+    li.addEventListener('dblclick', function() {
+        let locationInDB = ref(database, `groceryList/${itemID}`);
+
+        remove(locationInDB);
+    });
+
+    // append to parent (list)
+    shoppingList.appendChild(li);
 }
 
 function clearTextBox(textboxToClear) {
@@ -42,11 +58,32 @@ function clearShoppingList() {
     shoppingList.innerHTML = "";
 }
 
+// runs every time there is a change in the DB
 onValue(groceryItemsInDB, function(snapshot) { // snapshot is current data in db
-    const itemsArray = Object.values(snapshot.val()); 
-    clearShoppingList();
-    for (let i = 0; i < itemsArray.length; i++) {
-        addToShoppingList(itemsArray[i]);
+
+    if (snapshot.exists()) {
+        // get the key-value pairs of data if it exists
+        const itemsArray = Object.entries(snapshot.val()); 
+
+        // clear old shopping list
+        clearShoppingList();
+
+        // iterate over new data
+        for (let i = 0; i < itemsArray.length; i++) {
+            // get current item
+            let currentItem = itemsArray[i];
+
+            // get current item's key
+            let currentItemID = currentItem[0];
+
+            // get current item's value
+            let currentItemValue = currentItem[1];
+
+            addToShoppingList(currentItem);
+        }
+    } else {
+        shoppingList.innerHTML = "No items here... yet!"
     }
+    
 });
 
